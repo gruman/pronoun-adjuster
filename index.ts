7ca90ad10e6d9279189789778e85763282603685
Matthew Gruman <i@gruman.co>
2024-10-24 13:44:51 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-const adjustPronouns = (text: string, pronoun1: string, pronoun2?: string): string => {
+const adjustPronouns = (text: string, pronoun1: string, pronoun2: string, name?: string): string => {
   return text
     .replace(/\[P1\]/g, (match, offset, fullText) => {
       const isStart = offset === 0 || /[.?!]\s{0,3}$/.test(fullText.slice(0, offset));
       return isStart ? pronoun1.charAt(0).toUpperCase() + pronoun1.slice(1) : pronoun1.toLowerCase();
     })
-    .replace(/\[P2\]/g, pronoun2 ? pronoun2.toLowerCase() : '');
+    .replace(/\[P2\]/g, pronoun2 ? pronoun2.toLowerCase() : '')
+    .replace(/\[NAME\]/g, name ? name : '');
 };
 
 export default adjustPronouns;
